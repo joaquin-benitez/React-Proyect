1d23686aa6b3299cd106b50e244b8456cb1c4f74
Joaquin Benitez <joaquincdelu@gmail.com>
2022-09-22 19:02:08 -0300

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './Components/Navbar';
 import './App.css';
 import Contador from './Components/Contador';
+import Card from './Components/Card';
 
 const App = () => {
     return (
         <>
            <Navbar/>      
            <Contador/>
+           <Card/>
         </>
     );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,29 @@ import React from 'react';
 const Card = () => {
     return (
         <>
-            
+            <div className="card">
+        <div className="servicio">            
+          <div className="img-conteiner">
+            <img className="img-serv" src="/ImgSpa/piedras.jpg" alt="piedras" />
+          </div>
+          <div className="servicio_pie">
+            <h3>Masaje con piedras</h3>
+            <hr />
+            <div className="rating">
+              <i className="bx bxs-star" />
+              <i className="bx bxs-star" />
+              <i className="bx bxs-star" />
+              <i className="bx bxs-star" />
+              <i className="bx bx-star" />
+            </div>
+            <div className="precio">$1200</div>
+          </div>
+          <div className="bottom">
+            <div className="btn__group">
+              <a href="#" className="btn addToCart">Añadir al Carrito</a>
+            </div>
+          </div>
+        </div></div>
         </>
     );
 }
