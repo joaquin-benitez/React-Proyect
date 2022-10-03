be82fe0ffecb79e05b5b5119ff61cbd93ebd5c43
Joaquin Benitez <joaquincdelu@gmail.com>
2022-10-03 18:36:04 -0300

    actualizacion

--- src/Components/Servicio.jsx
+++ src/Components/Servicio.jsx
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import { consultarBDD } from '../Utilidades/FuncionesUtiles';
-import DetalleServicios from './DetalleServicios'
+import { consultarBDD } from './Utilidades/FuncionesUtiles';
+import DetalleServicios from './DetalleServicios';
 
 
 const Servicio = () => {
   const [servicio, setServicio] = useState([]);
   const {id} = useParams()
   useEffect(() => {
-     consultarBDD('../json/servicios.json').then(servicios => {
+     consultarBDD('./json/servicios.json').then(servicios => {
       const servicio1 = servicios.find(servicioArray => servicioArray.id == id)
       console.log(servicios)
       setServicio(servicio1)
